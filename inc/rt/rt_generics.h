2aad8b04c65ea9fe97637a259aae2e2f5b39dd5d
Leo Kuznetsov <leo.kuznetsov@gmail.com>
2024-10-15 15:28:45 -0700

    Better C23 generics

--- inc/rt/rt_generics.h
+++ inc/rt/rt_generics.h
@@ -1,86 +1,93 @@
 #ifndef rt_generics_header_included
 #define rt_generics_header_included
 
-#include <stdint.h>
+#if __has_include("alloca.h") // alloca()
+#include <alloca.h>
+#elif __has_include("malloc.h")
 #include <malloc.h>
+#endif
+
+#include <float.h>
+#include <string.h>  // memcpy()
 
 typedef float  fp32_t;
 typedef double fp64_t;
 
-// Beware that "Type names ending with a _t are reserved by
-// The Open Group Base Specifications Issue 7, 2018 edition
-// IEEE Std 1003.1-2017 (Revision of IEEE Std 1003.1-2008)"
-// https://pubs.opengroup.org/onlinepubs/9699919799/functions/V2_chap02.html
-// and this header might break in 2099 when Open Group decides to
-// have name better than "double" and "float" to a couple of fundamental
-// compute type (maybe along with fp8_t fp16_t and bf16_t etc)
+#if DBL_DIG < LDBL_DIG
+typedef long double fp80_t; // TODO: rt_implement and rt_dispatch
+#endif
 
-static inline int8_t   rt_max_int8(int8_t x, int8_t y)       { return x > y ? x : y; }
-static inline int16_t  rt_max_int16(int16_t x, int16_t y)    { return x > y ? x : y; }
-static inline int32_t  rt_max_int32(int32_t x, int32_t y)    { return x > y ? x : y; }
-static inline int64_t  rt_max_int64(int64_t x, int64_t y)    { return x > y ? x : y; }
-static inline uint8_t  rt_max_uint8(uint8_t x, uint8_t y)    { return x > y ? x : y; }
-static inline uint16_t rt_max_uint16(uint16_t x, uint16_t y) { return x > y ? x : y; }
-static inline uint32_t rt_max_uint32(uint32_t x, uint32_t y) { return x > y ? x : y; }
-static inline uint64_t rt_max_uint64(uint64_t x, uint64_t y) { return x > y ? x : y; }
-static inline fp32_t   rt_max_fp32(fp32_t x, fp32_t y)       { return x > y ? x : y; }
-static inline fp64_t   rt_max_fp64(fp64_t x, fp64_t y)       { return x > y ? x : y; }
+inline void rt_cannot_dispatch(void) {
+    static void (*crash)(void); // == 0
+    crash(); // this should not be called
+}
 
-// MS cl.exe version 19.39.33523 has issues with "long":
-// does not pick up int32_t/uint32_t types for "long" and "unsigned long"
-// need to handle long / unsigned long separately:
+#define rt_max_(n, t) inline t rt_max_ ## n(t x, t y) { return x > y ? x : y; }
+#define rt_min_(n, t) inline t rt_min_ ## n(t x, t y) { return x < y ? x : y; }
 
-static inline long          rt_max_long(long x, long y)                    { return x > y ? x : y; }
-static inline unsigned long rt_max_ulong(unsigned long x, unsigned long y) { return x > y ? x : y; }
+#define rt_implement_for_scalar_types(f) \
+    f(char, char) \
+    f(short_int, short int) \
+    f(int, int) \
+    f(long_int, long int) \
+    f(long_long_int, long long int) \
+    f(unsigned_char, unsigned char) \
+    f(unsigned_short_int, unsigned short int) \
+    f(unsigned_int, unsigned int) \
+    f(unsigned_long_int, unsigned long int) \
+    f(unsigned_long_long_int, unsigned long long int) \
+    f(float, float) \
+    f(double, double)
 
-static inline int8_t   rt_min_int8(int8_t x, int8_t y)       { return x < y ? x : y; }
-static inline int16_t  rt_min_int16(int16_t x, int16_t y)    { return x < y ? x : y; }
-static inline int32_t  rt_min_int32(int32_t x, int32_t y)    { return x < y ? x : y; }
-static inline int64_t  rt_min_int64(int64_t x, int64_t y)    { return x < y ? x : y; }
-static inline uint8_t  rt_min_uint8(uint8_t x, uint8_t y)    { return x < y ? x : y; }
-static inline uint16_t rt_min_uint16(uint16_t x, uint16_t y) { return x < y ? x : y; }
-static inline uint32_t rt_min_uint32(uint32_t x, uint32_t y) { return x < y ? x : y; }
-static inline uint64_t rt_min_uint64(uint64_t x, uint64_t y) { return x < y ? x : y; }
-static inline fp32_t   rt_min_fp32(fp32_t x, fp32_t y)       { return x < y ? x : y; }
-static inline fp64_t   rt_min_fp64(fp64_t x, fp64_t y)       { return x < y ? x : y; }
+#define rt_implement_for_pointer_types(f) \
+    f(char_ptr, char*) \
+    f(short_int_ptr, short int*) \
+    f(int_ptr, int*) \
+    f(long_int_ptr, long int*) \
+    f(long_long_int_ptr, long long int*) \
+    f(unsigned_char_ptr, unsigned char*) \
+    f(unsigned_short_int_ptr, unsigned short int*) \
+    f(unsigned_int_ptr, unsigned int*) \
+    f(unsigned_long_int_ptr, unsigned long int*) \
+    f(unsigned_long_long_int_ptr, unsigned long long int*) \
+    f(float_ptr, float*) \
+    f(double_ptr, double*)
 
-static inline long          rt_min_long(long x, long y)                    { return x < y ? x : y; }
-static inline unsigned long rt_min_ulong(unsigned long x, unsigned long y) { return x < y ? x : y; }
+rt_implement_for_scalar_types(rt_max_)
+rt_implement_for_scalar_types(rt_min_)
+rt_implement_for_pointer_types(rt_max_)
+rt_implement_for_pointer_types(rt_min_)
 
+#define rt_dispatch_for_scalar_types(f, x, y) _Generic((x),     \
+    char*:                   f ## _char_ptr,                    \
+    short int*:              f ## _short_int_ptr,               \
+    int*:                    f ## _int_ptr,                     \
+    long int*:               f ## _long_int_ptr,                \
+    long long int*:          f ## _long_long_int_ptr,           \
+    unsigned char*:          f ## _unsigned_char_ptr,           \
+    unsigned short int*:     f ## _unsigned_short_int_ptr,      \
+    unsigned int*:           f ## _unsigned_int_ptr,            \
+    unsigned long int*:      f ## _unsigned_long_int_ptr,       \
+    unsigned long long int*: f ## _unsigned_long_long_int_ptr,  \
+    float*:                  f ## _float_ptr,                   \
+    double*:                 f ## _double_ptr,                  \
+    default:                 _Generic((x) - (y),                \
+        char:                   f ## _char,                     \
+        short int:              f ## _short_int,                \
+        int:                    f ## _int,                      \
+        long int:               f ## _long_int,                 \
+        long long int:          f ## _long_long_int,            \
+        unsigned char:          f ## _unsigned_char,            \
+        unsigned short int:     f ## _unsigned_short_int,       \
+        unsigned int:           f ## _unsigned_int,             \
+        unsigned long int:      f ## _unsigned_long_int,        \
+        unsigned long long int: f ## _unsigned_long_long_int,   \
+        float:                  f ## _float,                    \
+        double:                 f ## _double,                   \
+        default:                rt_cannot_dispatch))(x, y)
 
-static inline void rt_min_undefined(void) { }
-static inline void rt_max_undefined(void) { }
-
-#define rt_max(X, Y) _Generic((X) + (Y), \
-    int8_t:   rt_max_int8,   \
-    int16_t:  rt_max_int16,  \
-    int32_t:  rt_max_int32,  \
-    int64_t:  rt_max_int64,  \
-    uint8_t:  rt_max_uint8,  \
-    uint16_t: rt_max_uint16, \
-    uint32_t: rt_max_uint32, \
-    uint64_t: rt_max_uint64, \
-    fp32_t:   rt_max_fp32,   \
-    fp64_t:   rt_max_fp64,   \
-    long:     rt_max_long,   \
-    unsigned long: rt_max_ulong, \
-    default:  rt_max_undefined)(X, Y)
-
-#define rt_min(X, Y) _Generic((X) + (Y), \
-    int8_t:   rt_min_int8,   \
-    int16_t:  rt_min_int16,  \
-    int32_t:  rt_min_int32,  \
-    int64_t:  rt_min_int64,  \
-    uint8_t:  rt_min_uint8,  \
-    uint16_t: rt_min_uint16, \
-    uint32_t: rt_min_uint32, \
-    uint64_t: rt_min_uint64, \
-    fp32_t:   rt_min_fp32,   \
-    fp64_t:   rt_min_fp64,   \
-    long:     rt_min_long,   \
-    unsigned long: rt_min_ulong, \
-    default:  rt_min_undefined)(X, Y)
-
+#define rt_max(x, y) rt_dispatch_for_scalar_types(rt_max, x, y)
+#define rt_min(x, y) rt_dispatch_for_scalar_types(rt_min, x, y)
 
 #if defined(__clang__) || defined(__GNUC__)
     #define rt_alloca(n)                                       \
@@ -98,13 +105,7 @@ static inline void rt_max_undefined(void) { }
     #define rt_alloca(n) alloca(n) // fallback
 #endif
 
-#define rt_swap_scalar(T, a, b) do { \
-    T tmp = (a);                     \
-    (a) = (b);                       \
-    (b) = tmp;                       \
-} while (0)
-
-static inline void rt_swap_implementation(void* a, void *b, size_t bytes) {
+static inline void rt_swap_(void* a, void *b, size_t bytes) {
     void* swap = rt_alloca(bytes);
     memcpy(swap,  a, bytes);
     memcpy(a, b, bytes);
@@ -112,18 +113,19 @@ static inline void rt_swap_implementation(void* a, void *b, size_t bytes) {
 }
 
 #define rt_swap(a, b) _Generic((a), \
-    int8_t:   rt_swap_implementation(&a, &b, sizeof(int8_t)),   \
-    int16_t:  rt_swap_implementation(&a, &b, sizeof(int16_t)),  \
-    int32_t:  rt_swap_implementation(&a, &b, sizeof(int32_t)),  \
-    int64_t:  rt_swap_implementation(&a, &b, sizeof(int64_t)),  \
-    uint8_t:  rt_swap_implementation(&a, &b, sizeof(uint8_t)),  \
-    uint16_t: rt_swap_implementation(&a, &b, sizeof(uint16_t)), \
-    uint32_t: rt_swap_implementation(&a, &b, sizeof(uint32_t)), \
-    uint64_t: rt_swap_implementation(&a, &b, sizeof(uint64_t)), \
-    fp32_t:   rt_swap_implementation(&a, &b, sizeof(fp32_t)),   \
-    fp64_t:   rt_swap_implementation(&a, &b, sizeof(fp64_t)),   \
-    long:     rt_swap_implementation(&a, &b, sizeof(long)),     \
-    unsigned long: rt_swap_implementation(&a, &b, sizeof(unsigned long)), \
-    default:  rt_swap_implementation(&a, &b, sizeof(a)))
+    char:                   rt_swap_(&a, &b, sizeof(char)),                   \
+    unsigned char:          rt_swap_(&a, &b, sizeof(unsigned char)),          \
+    short int:              rt_swap_(&a, &b, sizeof(short int)),              \
+    unsigned short int:     rt_swap_(&a, &b, sizeof(unsigned short int)),     \
+    int:                    rt_swap_(&a, &b, sizeof(int)),                    \
+    unsigned int:           rt_swap_(&a, &b, sizeof(unsigned int)),           \
+    long int:               rt_swap_(&a, &b, sizeof(long int)),               \
+    unsigned long int:      rt_swap_(&a, &b, sizeof(unsigned long int)),      \
+    long long int:          rt_swap_(&a, &b, sizeof(long long int)),          \
+    unsigned long long int: rt_swap_(&a, &b, sizeof(unsigned long long int)), \
+    float:                  rt_swap_(&a, &b, sizeof(float)),                  \
+    double:                 rt_swap_(&a, &b, sizeof(double)),                 \
+    long double:            rt_swap_(&a, &b, sizeof(long double)),            \
+    default:                rt_swap_(&a, &b, sizeof(a)))
 
 #endif // rt_generics_header_included
