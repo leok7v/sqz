94fd86fca300fcfca853b353dc9095b4ccc49014
Leo Kuznetsov <leo.kuznetsov@gmail.com>
2024-10-31 00:08:34 -0700

    H2 prefixes are working

--- h2.c
+++ h2.c
@@ -27,17 +27,73 @@ static void init(struct lz77 *lz, const uint8_t* in, size_t n, size_t window) {
     lz->w  = window;
 }
 
+#ifdef verbose
+#define trace(...) printf(__VA_ARGS__)
+#else
+#define trace(...) ((void)0)
+#endif
+
+static void chain(struct lz77 *lz, size_t index) {
+    const uint8_t* in = lz->in;
+    const size_t i = lz->i;
+    const size_t w = lz->w;
+    printf("[%zd] \"%c%c\" index(i mod %zd)=%zu ", i, in[i], in[i + 1], w, index);
+    size_t p = i;
+    size_t d = lz->prev[index];
+    while (d > 0 && d <= p && p - d < i && i <= p - d + w) {
+        p -= d;
+        printf(" \"%c%c\" %zu", in[p], in[p + 1], p);
+        d = lz->prev[p % w];
+    }
+    printf("\n");
+}
+
+static void verify_chain(struct lz77 *lz, size_t index) {
+    const uint8_t* in = lz->in;
+    const size_t i = lz->i;
+    const size_t w = lz->w;
+    size_t p = i;
+    size_t d = lz->prev[index];
+    while (d > 0 && d <= p && p - d < i && i <= p - d + w) {
+        p -= d;
+        assert(in[p] == in[i] && in[p + 1] == in[i + 1]);
+        d = lz->prev[p % w];
+    }
+}
+
 static void insert(struct lz77* lz) {
     const uint8_t* in = lz->in;
     const size_t   n = lz->n;
     const size_t   i = lz->i;
-    if (i < n - 2) {
+    const size_t   w = lz->w;
+    if (i < n - 1) {
         const uint16_t prefix = in[i] | (in[i + 1] << 8);
         const size_t index = i % lz->w;
-//      printf("[%zd] prev[%zd] := %zu\n", i, index, lz->map2[prefix]);
-        lz->prev[index]  = lz->map2[prefix];
+        size_t pp = lz->map2[prefix]; // previous position
+        trace("[%zd] map[\"%c%c\":0x%04x(%u)] = %zu prev[%zd] := ",
+               i, in[i], in[i + 1], prefix, prefix, pp, index);
+        if (pp == 0) {
+            trace("0\n");
+            lz->prev[index] = 0;
+        } else {
+            pp--;
+            if (i - pp <= w) {
+                assert(in[pp] == in[i] && in[pp + 1] == in[i + 1]);
+                assert(pp < i);
+                trace("%zd\n", i - pp);
+                lz->prev[index] = i - pp;
+            } else {
+                trace("0 (out of window)\n");
+                lz->prev[index] = 0;
+            }
+        }
         lz->map2[prefix] = i + 1;
-//      printf("[%zd] map2[\"%c%c\":0x%04x(%u)] := %zu\n", i, in[i+1], in[i], prefix, prefix, index + 1);
+        trace("[%zd] map2[\"%c%c\":0x%04x(%u)] := %zu\n",
+               i, in[i], in[i + 1], prefix, prefix, index + 1);
+        #ifdef verbose
+        chain(lz, index);
+        #endif
+        verify_chain(lz, index);
     }
 }
 
@@ -51,12 +107,14 @@ static inline void lz77_find_longest_match(struct lz77* lz, size_t *ml, size_t *
     const size_t n = lz->n;
     const size_t i = lz->i;
     const size_t w = lz->w;
-    if (1 <= i && i < n - 2) {
+    const uint8_t* in = lz->in;
+    trace("[%zd] \"%c%c\"\n", i, in[i], in[i + 1]);
+    if (1 <= i && i < n - 1) {
+//      if (i == 15 && w == 8) rt_breakpoint();
         size_t len = 0;
         size_t dst = 0;
-        const uint8_t* in = lz->in;
         size_t p = lz->map2[in[i] | (in[i + 1] << 8)];
-        if (0 < p && i - (p - 1) <= w) {
+        if (0 < p && p - 1 < i && i <= p - 1 + w) {
             p--;
             assert(p < i && i - p <= w);
             size_t max_k = n - i > max_len ? max_len : n - i;
@@ -66,9 +124,10 @@ static inline void lz77_find_longest_match(struct lz77* lz, size_t *ml, size_t *
                 len = k;
                 dst = i - p;
             }
-            while (lz->prev[p] > 0 && i - lz->prev[p] <= w) {
-                p = lz->prev[p];
-                if (i - p > w) { break; }
+            size_t d = lz->prev[p % w];
+            while (d > 0 && d <= p && p - d < i && i <= p - d + w) {
+                p -= d;
+                assert(in[p] == in[i] && in[p + 1] == in[i + 1]);
                 if (memcmp(in + p, in + i, len) == 0) {
                     k = 2;
                     while (k < max_k && in[p + k] == in[i + k]) { k++; }
@@ -77,6 +136,7 @@ static inline void lz77_find_longest_match(struct lz77* lz, size_t *ml, size_t *
                         dst = i - p;
                     }
                 }
+                d = lz->prev[p % w];
             }
             *ml = len;
             *md = dst;
@@ -90,15 +150,14 @@ static void lz77_linear_search(struct lz77* lz, size_t *ml, size_t *md) {
     const size_t n = lz->n;
     const size_t i = lz->i;
     const size_t w = lz->w;
-    if (1 < i && i < n - 2) {
-//      if (i == 13) rt_breakpoint();
+    if (1 <= i && i < n - 1) {
         size_t len = 0;
         size_t dst = 0;
         const uint8_t* in = lz->in;
         size_t j = i - 1;
         size_t min_j = i >= w ? i - w : 0;
         for (;;) {
-            size_t max_k = n > max_len ? max_len : n;
+            size_t max_k = n - i > max_len ? max_len : n - i;
             size_t k = 0;
             while (k < max_k && in[j + k] == in[i + k]) { k++; }
             if (k >= 2 && k > len) {
@@ -117,11 +176,11 @@ static void lz77_linear_search(struct lz77* lz, size_t *ml, size_t *md) {
 static int test(struct lz77* lz, const uint8_t* in, const size_t n, const size_t w) {
     init(lz, in, strlen((char*)in), w);
     printf("window: %zd\n", w);
-    printf("%.*s: %zu\n", (int)n, in, n);
+    printf("\"%.*s: %zu\"\n ", (int)n, in, n);
     for (size_t i = 0; i < n; i++) {
         printf("%d", i % 10);
     }
-    printf("\n");
+    printf("\n ");
     for (size_t i = 0; i < n; i++) {
         printf("%c", i % 10 == 0 ? '0' + (i / 10) % 10 : 0x20);
     }
@@ -129,7 +188,7 @@ static int test(struct lz77* lz, const uint8_t* in, const size_t n, const size_t
     lz->i = 0;
     insert(lz);
     while (lz->i < lz->n) {
-        if (1 <= lz->i && lz->i < lz->n - 2) {
+        if (1 <= lz->i && lz->i < lz->n - 1) {
             size_t ml1 = 0, md1 = 0;
             lz77_find_longest_match(lz, &ml1, &md1);
             size_t ml2= 0, md2 = 0;
@@ -140,7 +199,7 @@ static int test(struct lz77* lz, const uint8_t* in, const size_t n, const size_t
                     printf("[%zd] linear  %zd:%zd \"%.2s\" \n", lz->i, ml2, md2, lz->in + lz->i);
                 }
                 assert(ml1 == ml2 && md1 == md2);
-
+                if (ml1 != ml2 || md1 != md2) { return 1; }
                 size_t next_i = lz->i + ml1;
                 while (lz->i < next_i) {
                     insert(lz);
@@ -157,15 +216,69 @@ static int test(struct lz77* lz, const uint8_t* in, const size_t n, const size_t
     return 0;
 }
 
-int main(int argc, const char* argv[]) {
-    (void)argc; (void)argv; // unused
-    static struct lz77 lz77;
-    struct lz77* lz = &lz77;
-    const char* in = "aa_aa_aaa_aaaa__aaaaa__";
+static int test1(struct lz77* lz) {
+    const char* in = "aaaaa";
+    return test(lz, (const uint8_t*)in, strlen(in), 2);
+}
+
+static int test2(struct lz77* lz) {
+    const char* in = "a_aa_aa_aaa_aaaa_aaaaa_aaaa_aaa_aa_a";
     const size_t n = strlen(in);
     size_t max_w = n < max_window ? n : max_window;
     for (size_t w = 2; w < max_w; w += w) {
         if (test(lz, (const uint8_t*)in, strlen(in), w)) { return 1; }
     }
     return 0;
+}
+
+static uint64_t seed = 1; // random seed start value (must be odd)
+
+static uint64_t random64(uint64_t* state) {
+    // Linear Congruential Generator with inline mixing
+    thread_local static bool initialized;
+    if (!initialized) { initialized = true; *state |= 1; };
+    *state = (*state * 0xD1342543DE82EF95uLL) + 1;
+    uint64_t z = *state;
+    z = (z ^ (z >> 32)) * 0xDABA0B6EB09322E3uLL;
+    z = (z ^ (z >> 32)) * 0xDABA0B6EB09322E3uLL;
+    return z ^ (z >> 32);
+}
+
+static double rand64(uint64_t *state) { // [0.0..1.0) exclusive to 1.0
+    return (double)random64(state) / ((double)UINT64_MAX + 1.0);
+}
+
+static int test3(struct lz77* lz) {
+    enum { N = 16, M = 256, T = 32 };
+    int pl[N] = { 0 };
+    for (int t = 0; t < T; t++) {
+        for (int i = 0; i < 16; i++) {
+            pl[i] = 2 + (int)(rand64(&seed) * 2);
+        }
+        uint8_t in[M] = {0};
+        int k = 0;
+        int j = 0;
+        while (k < M - 2) {
+            for (int i = 0; i < pl[j] && k < M - 2; i++) {
+                in[k++] = 'a';
+            }
+            j = (j + 1) % N;
+            if (k < M - 2) { in[k++] = '_'; }
+        }
+        assert(in[sizeof(in) - 1] == 0);
+        const size_t n = strlen(in);
+        size_t max_w = n < max_window ? n : max_window;
+        for (size_t w = 2; w < max_w; w += w) {
+            if (test(lz, (const uint8_t*)in, strlen(in), w)) { return 1; }
+        }
+    }
+    return 0;
+}
+
+int main(int argc, const char* argv[]) {
+    (void)argc; (void)argv; // unused
+    static struct lz77 lz77;
+    struct lz77* lz = &lz77;
+//  return test1(lz) || test2(lz);
+    return test3(lz);
 }
